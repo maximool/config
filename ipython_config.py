e0bc4e413838e486c034c465ea8441a9daecc001
Maxime Margerin <maxime.margerin@gmail.com>
2016-07-30 18:17:53 +0200

    Added some options for iPython

---
+++ ipython_config.py
@@ -0,0 +1,6 @@
+#####################
+# Ajouts personnels #
+#####################
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.editor = "/bin/vim"
+c.TerminalIPythonApp.display_banner = False
